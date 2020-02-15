e97e55000109246728b01bccaec4837f9f073cb5
Manish <manishkumar13899@gmail.com>
2020-02-15 17:21:15 +0530

    graph done

--- pages/graph-vis/graph.js
+++ pages/graph-vis/graph.js
@@ -1,12 +1,12 @@
 import Graph from "react-graph-vis";
-// import Graph from "../../lib";
-
-// import Graph from 'react-graph-vis'
-
+import factory from '../../ethereum/factory'
 import React, {Component} from "react";
 // import { render } from "react-dom";
-let graph,options,events;
+import Fund from '../../ethereum/fund';
+
+
 class graphs extends Component{
+    
     state={
         loading:true,
         nodes:[],
@@ -14,20 +14,32 @@ class graphs extends Component{
         options:{},
         events:{}
     };
-
-    componentDidMount(){
-        const graph = {
-            nodes: [
-                { id: 1, label: "Node 1", color: "#e04141" },
-                { id: 2, label: "Node 2", color: "#e09c41" },
-                { id: 3, label: "Node 3", color: "#e0df41" },
-                { id: 4, label: "Node 4", color: "#7be041" },
-                { id: 5, label: "Node 5", color: "#41e0c9" }
-            ],
-            edges: [{ from: 1, to: 2 }, { from: 1, to: 3 }, { from: 2, to: 4 }, { from: 2, to: 5 }]
-        };
-
-		const options = {
+    constructor(props){
+        super(props)
+        
+        
+        
+    }
+    static async getInitialProps(){
+        let color = ["#e04141","#e09c41","#e0df41","#7be041","#41e0c9"]
+        let finalNodes = []
+        finalNodes.push({id:0x81835bfB922eB03d12517CbEEB403E6b1C68e8fb,label:'Factory ',color:color[finalNodes.length%5]})
+        let tempNodes = []
+        tempNodes.push(0x81835bfB922eB03d12517CbEEB403E6b1C68e8fb)
+        let finalEdges =[]
+        const rootNodes = await factory.methods.getDeployedRoots().call()
+        rootNodes.map(address => {
+            finalEdges.push({from:tempNodes[0],to:address})
+            finalNodes.push({id:address,label:'Node '+finalNodes.length,color:color[finalNodes.length%5]})
+            tempNodes.push(address)
+        })
+        tempNodes.shift()
+        return {rootNodes ,color,finalEdges,finalNodes,tempNodes}
+    }
+    
+    async componentDidMount(){
+        
+        const options = {
             layout: {
                 hierarchical: false
             },
@@ -45,12 +57,27 @@ class graphs extends Component{
                 console.log(edges);
             }
         };
-        this.setState({nodes:graph.nodes,edges:graph.edges,options:options,events:events,loading:false})
+        while(this.props.tempNodes.length)
+        {
+            const fund = Fund(this.props.tempNodes[0])
+            const childNodes = await fund.methods.getChildFunds().call()
+            childNodes.map(address => {
+                this.props.finalEdges.push({from:this.props.tempNodes[0],to:address})
+                this.props.finalNodes.push({id:address,label:'Node '+this.props.finalNodes.length,color:this.props.color[this.props.finalNodes.length%5]})
+                this.props.tempNodes.push(address)
+            })
+            this.props.tempNodes.shift()
+            this.setState({nodes:this.props.finalNodes,edges:this.props.finalEdges,options:options,events:events,loading:false})
+        }
+        
+		
+        
         
     }
     
 renderGraph = ()=>{
     console.log('Heloo')
+    
     const graph1={nodes:this.state.nodes,edges:this.state.edges}
     return <Graph graph={graph1} options={this.state.options} events={this.state.events} style={{ height: "640px" }} />
 }
@@ -63,16 +90,10 @@ render(){
         {this.renderGraph()}
     </div>
     // document.getElementById("root")
-    )
-}
+    )}
     else{
-        return(
-            <div>
-                <h1>React graph vis</h1>
-                Loading
-            </div>
-            // document.getElementById("root")
-            )
+       return( <div>Loading</div>)
+
     }
 }
 }
