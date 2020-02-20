4c9a30871e781cbad3dae6ee78ff0b8235133f32
Aritra Chatterjee <aritrachatterjee07@gmail.com>
2020-02-20 11:41:48 +0530

    Improved graph

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,16 @@ import Fund from '../ethereum/fund'
 import Graph from "react-graph-vis";
 
 class FundManagerIndex extends Component {
-	state={
-        
+	state = {
+
 		loadingButton: false,
 		loadingButton1: false
 	};
 
-	
-	
-	
-	
+
+
+
+
 
 
 
@@ -30,66 +30,65 @@ class FundManagerIndex extends Component {
 		// Router.pushRoute('/graph-vis/graph');
 	}
 
-	
+
 
 	render() {
-		
-		return (
-			
-			<Layout
-			render={({setLoading,setNotLoading}) => (
-				<Segment placeholder raised style={{ marginTop: 50 }}>
-					<Grid columns={2} stackable textAlign='center'>
-						<Divider vertical> => </Divider>
 
-						<Grid.Row verticalAlign='middle'>
-							<Grid.Column>
-								<Header icon>
-									<Icon name='factory' />
+		return (
+
+			<Layout
+				render={({ setLoading, setNotLoading }) => (
+					<Segment placeholder raised style={{ marginTop: 50 }}>
+						<Grid columns={2} stackable textAlign='center'>
+							<Divider vertical> => </Divider>
+
+							<Grid.Row verticalAlign='middle'>
+								<Grid.Column>
+									<Header icon>
+										<Icon name='factory' />
 										Factory Functionalities
 				          		</Header>
-								<Link route='/factory/rootFund'>
-									<a>
-										<Button 
-											primary
-											content='Enter'
-											loading={this.state.loadingButton}
-											onClick={this.onClick}
-										/>
-									</a>
-								</Link>
-							</Grid.Column>
+									<Link route='/factory/rootFund'>
+										<a>
+											<Button
+												primary
+												content='Enter'
+												loading={this.state.loadingButton}
+												onClick={this.onClick}
+											/>
+										</a>
+									</Link>
+								</Grid.Column>
 
-							<Grid.Column>
-							<Header icon>
-									<Icon name='code branch' />
+								<Grid.Column>
+									<Header icon>
+										<Icon name='code branch' />
 										Show current status of funds
 				          		</Header>
-								 <Link route='/graph-vis/graph'>
-									<a> 
-										<Button 
-											primary
-											content='Show'
-											loading={this.state.loadingButton1}
-											onClick={this.onClick1}
-										/>
-									</a>
-								</Link> 
-								{/* <div>{!this.state.loading?this.renderGraph(setLoading):''}</div>
+									<Link route='/graph-vis/graph'>
+										<a>
+											<Button
+												primary
+												content='Show'
+												loading={this.state.loadingButton1}
+												onClick={this.onClick1}
+											/>
+										</a>
+									</Link>
+									{/* <div>{!this.state.loading?this.renderGraph(setLoading):''}</div>
 								<br />
 								<div>{this.state.flag?this.state.node:''}</div>
 								<div>{this.state.flag1?this.state.nodes:''}</div> */}
-							</Grid.Column>
-						</Grid.Row>
-					</Grid>
-				</Segment>
-			)}
+								</Grid.Column>
+							</Grid.Row>
+						</Grid>
+					</Segment>
+				)}
 			/>
-			
+
 		);
 
 
 	}
 }
 export default FundManagerIndex;
-	
